b5805e7dcbe7077225ba2698643fd9cba5344c77
stephen_wang <1402020165@qq.com>
2017-09-14 15:20:01 +0800

    基本动画

--- BaseAnimationDemo/BaseAnimationDemo/ViewController.m
+++ BaseAnimationDemo/BaseAnimationDemo/ViewController.m
@@ -23,12 +23,30 @@
    // [self transformAnimation];
    // [self fadeAnimation];
    // [self rotationAnimation];
-    [self transform3DAnimation];
+   // [self transform3DAnimation];
+    
+  
+    
+    
 }
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+   // [self boundsAniamtion];
+    //[self scaleAnimation];
+    
+   // [self positionAnimation];
+   // [self bgColorAnimation];
+    
+    //[self cornerRadiusAnimation];
+    
+   // [self borderAnimation];
+    
+    [self shadowColorAnimation];
+}
+
 //MARK: - getter
 - (UIView *)myView {
     if (!_myView) {
-        _myView = [[UIView alloc]initWithFrame:CGRectMake(100, 100, 50, 50)];
+        _myView = [[UIView alloc]initWithFrame:CGRectMake(100, 100, 80, 80)];
         _myView.backgroundColor = [UIColor yellowColor];
         [self.view addSubview:_myView];
     }
@@ -45,27 +63,110 @@
 
 //MARK: - 位移动画
 - (void)positionAnimation {
-    CABasicAnimation *positionAniamtion = [CABasicAnimation animationWithKeyPath:@"position"];
-    positionAniamtion.fromValue = [NSValue valueWithCGPoint:CGPointMake(100, 100)];
-    positionAniamtion.toValue = [NSValue valueWithCGPoint:CGPointMake(300, 100)];
+    /*  注释部分于下面代码效果相同
+     CABasicAnimation *positionAniamtion = [CABasicAnimation animationWithKeyPath:@"position"];
+     positionAniamtion.fromValue = [NSValue valueWithCGPoint:CGPointMake(100, 100)];
+     positionAniamtion.toValue = [NSValue valueWithCGPoint:CGPointMake(300, 100)];
+     */
+    CABasicAnimation *positionAniamtion = [CABasicAnimation animationWithKeyPath:@"transform.translation.x"];
+    positionAniamtion.fromValue = @100;
+    positionAniamtion.toValue = @150;
     positionAniamtion.duration = 2.0;
+    positionAniamtion.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
     //动画完成后不删除
     positionAniamtion.removedOnCompletion = NO;
     //保持最新的状态
     positionAniamtion.fillMode = kCAFillModeForwards;
-    [self.myView.layer addAnimation:positionAniamtion forKey:@"position"];
+    [self.myView.layer addAnimation:positionAniamtion forKey:@"positionAniamtion"];
 }
 
 //MARK: - 缩放动画
-- (void)transformAnimation {
-    CABasicAnimation *transformAnimation = [CABasicAnimation animationWithKeyPath:@"transform"];
-    transformAnimation.fromValue = [NSValue valueWithCATransform3D:CATransform3DMakeScale(1.0, 1.0, 1.0)];
-    transformAnimation.toValue = [NSValue valueWithCATransform3D:CATransform3DMakeScale(1.5, 1.5, 1.0)];
-    transformAnimation.duration = 1.0;
-    transformAnimation.removedOnCompletion = NO;
-    transformAnimation.fillMode = kCAFillModeForwards;
-    [self.myView.layer addAnimation:transformAnimation forKey:@"transform"];
+- (void)scaleAnimation {
+    CABasicAnimation *scaleAnimation = [CABasicAnimation animationWithKeyPath:@"transform.scale"];
+    //scaleAnimation.fromValue = [NSValue valueWithCATransform3D:CATransform3DMakeScale(1.0, 1.0, 1.0)];
+    scaleAnimation.toValue = [NSValue valueWithCATransform3D:CATransform3DMakeScale(1.5, 1.5, 1.0)];
+    scaleAnimation.duration = 1.0;
+    scaleAnimation.removedOnCompletion = NO;
+    scaleAnimation.fillMode = kCAFillModeForwards;
+    [self.myView.layer addAnimation:scaleAnimation forKey:@"transformAnimation"];
 
+}
+//MARK： - bounds动画
+- (void)boundsAniamtion {
+    CABasicAnimation *boundsAniamtion = [CABasicAnimation animationWithKeyPath:@"bounds.size.width"];
+  
+    boundsAniamtion.toValue =  @(150);
+    boundsAniamtion.duration = 1.0;
+    boundsAniamtion.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+    boundsAniamtion.removedOnCompletion = NO;
+    boundsAniamtion.fillMode = kCAFillModeForwards;
+    [self.myView.layer addAnimation:boundsAniamtion forKey:@"size"];
+}
+
+//MARK: - 背景颜色
+- (void)bgColorAnimation {
+    
+    CABasicAnimation *bgColorAnimation = [CABasicAnimation animationWithKeyPath:@"backgroundColor"];
+    bgColorAnimation.toValue = (id)([UIColor redColor].CGColor);
+    bgColorAnimation.duration = 3.0;
+    bgColorAnimation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+    bgColorAnimation.removedOnCompletion = NO;
+    bgColorAnimation.fillMode = kCAFillModeForwards;
+    [self.myView.layer addAnimation:bgColorAnimation forKey:@"bgColorAnimation"];
+}
+//MARK: - 圆角动画
+- (void)cornerRadiusAnimation {
+    CABasicAnimation *cornerRadiusAnimation = [CABasicAnimation animationWithKeyPath:@"cornerRadius"];
+    cornerRadiusAnimation.toValue = @(40);
+    cornerRadiusAnimation.duration = 1.0;
+    cornerRadiusAnimation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+    cornerRadiusAnimation.removedOnCompletion = NO;
+    cornerRadiusAnimation.fillMode = kCAFillModeForwards;
+    [self.myView.layer addAnimation:cornerRadiusAnimation forKey:@"cornerRadiusAnimation"];
+}
+//MARK: - 边框动画
+- (void)borderAnimation {
+    CABasicAnimation *cornerRadiusAnimation = [CABasicAnimation animationWithKeyPath:@"cornerRadius"];
+    cornerRadiusAnimation.toValue = @(40);
+    cornerRadiusAnimation.duration = 1.0;
+    cornerRadiusAnimation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+    cornerRadiusAnimation.removedOnCompletion = NO;
+    cornerRadiusAnimation.fillMode = kCAFillModeForwards;
+ 
+    CABasicAnimation *borderWidthAnimation = [CABasicAnimation animationWithKeyPath:@"borderWidth"];
+    borderWidthAnimation.toValue = @(5.0);
+    borderWidthAnimation.duration = 1.0;
+    borderWidthAnimation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+    borderWidthAnimation.removedOnCompletion = NO;
+    borderWidthAnimation.fillMode = kCAFillModeForwards;
+    CABasicAnimation *borderColorAnimation = [CABasicAnimation animationWithKeyPath:@"borderColor"];
+    borderColorAnimation.toValue = (id)[UIColor purpleColor].CGColor;
+    borderColorAnimation.duration = 1.0;
+    borderColorAnimation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+    borderColorAnimation.removedOnCompletion = NO;
+    borderColorAnimation.fillMode = kCAFillModeForwards;
+    
+    CAAnimationGroup *group = [CAAnimationGroup animation];
+    group.animations = @[cornerRadiusAnimation, borderColorAnimation, borderWidthAnimation];
+    group.duration = 1.0;
+    group.repeatCount = MAXFLOAT;
+    group.removedOnCompletion = NO;
+    group.fillMode = kCAFillModeForwards;
+    
+    [self.myView.layer addAnimation:group forKey:nil];
+}
+//MARK: - 阴影颜色动画
+- (void)shadowColorAnimation {
+    CABasicAnimation *shadowColorAnimation = [CABasicAnimation animationWithKeyPath:@"shadowColor"];
+    shadowColorAnimation.fromValue = (id)[UIColor whiteColor].CGColor;
+    shadowColorAnimation.toValue = (id)[UIColor purpleColor].CGColor;
+    shadowColorAnimation.duration = 1.0;
+    shadowColorAnimation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+    shadowColorAnimation.removedOnCompletion = NO;
+    shadowColorAnimation.fillMode = kCAFillModeForwards;
+    
+    [self.myView.layer addAnimation:shadowColorAnimation forKey:@"shadowColorAnimation"];
+    
 }
 //MARK: - 透明度动画
 - (void)fadeAnimation {
@@ -76,7 +177,7 @@
     fadeAnimation.duration = 3.0;
     fadeAnimation.removedOnCompletion = NO;
     fadeAnimation.fillMode = kCAFillModeForwards;
-    [self.myView.layer addAnimation:fadeAnimation forKey:@"opacity"];
+    [self.myView.layer addAnimation:fadeAnimation forKey:@"fadeAnimation"];
     
 }
 
@@ -108,24 +209,14 @@
 //MARK: - 3D旋转动画
 - (void)transform3DAnimation {
     CATransform3D  rotate = CATransform3DMakeRotation(M_PI/6, 1, 0, 0);
-    self.myView.layer.transform = CATransform3d
+    //self.myView.layer.transform = CATransform3d
     [UIView animateWithDuration:3.0 animations:^{
-        self.myView.layer.transform = CATransform3DMakeRotation(M_PI, 0, 1, 1);
+        self.myView.layer.transform = CATransform3DMakeRotation(M_PI/2, 0, 1, 0);
     }];
+
     
 }
 
-//MARK: -  Using a Keyframe Animation to Change Layer Properties
-- (void)keyFrameAnimation {
-    CGMutablePathRef thePath = CGPathCreateMutable();
-    CGPathMoveToPoint(thePath, NULL, 74.0, 74.0);
-    CGPathAddCurveToPoint(thePath, NULL, 74.0, 500.0, 150.0, 300.0, 150.0, 74.0);
-    CGPathAddCurveToPoint(thePath, NULL, 150.0, 500.0, 566.0, 300.0, 320.0, 74.0);
-    CAKeyframeAnimation *theAniamtion = [CAKeyframeAnimation animationWithKeyPath:@"position"];
-    theAniamtion.path = thePath;
-    theAniamtion.duration = 5.0;
-    [self.myView.layer addAnimation:theAniamtion forKey:@"position"];
-}
 
 //MARK: - Animating Multiple Changes Together
 - (void)groupAnimation {
